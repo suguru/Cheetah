1e05f034e16ef5cd445bc88d7cb8ed64b30546a4
Suguru Namura <snamura@gmail.com>
2015-09-24 12:59:09 +0900

    Use cheetah.id when added to the manager

--- Cheetah/CheetahManager.swift
+++ Cheetah/CheetahManager.swift
@@ -39,7 +39,7 @@ public class CheetahManager {
         }
         
         // add to management dictionary
-        cheetahs[counter] = cheetah
+        cheetahs[cheetah.id] = cheetah
         
         // start run loop
         if displayLink == nil {
